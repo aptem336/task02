52eb26434e6a62ba5810e0c476caac8f9533a50d
Artem Popov <aptem336@gmail.com>
2020-05-15 01:15:34 +0800

    complete Conjunction Disjunction

--- src/main/java/lt/vu/mif/jate/tasks/task02/search/operation/Conjunction.java
+++ src/main/java/lt/vu/mif/jate/tasks/task02/search/operation/Conjunction.java
@@ -2,8 +2,14 @@ package lt.vu.mif.jate.tasks.task02.search.operation;
 
 /**
  * Conjunction.
+ *
  * @author valdo
  */
 public class Conjunction extends Junction {
 
+    @Override
+    public boolean execute(Object o) {
+        for (Operation operation : getOperations()) if (!operation.execute(o)) return false;
+        return true;
+    }
 }
--- src/main/java/lt/vu/mif/jate/tasks/task02/search/operation/Disjunction.java
+++ src/main/java/lt/vu/mif/jate/tasks/task02/search/operation/Disjunction.java
@@ -2,8 +2,14 @@ package lt.vu.mif.jate.tasks.task02.search.operation;
 
 /**
  * Disjunction operation.
+ *
  * @author valdo
  */
 public class Disjunction extends Junction {
 
+    @Override
+    public boolean execute(Object o) {
+        for (Operation operation : getOperations()) if (operation.execute(o)) return true;
+        return false;
+    }
 }
