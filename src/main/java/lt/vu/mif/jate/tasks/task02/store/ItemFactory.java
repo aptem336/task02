032c379a65f2d6c6867e1461adad398009673099
Artem Popov <aptem336@gmail.com>
2020-05-15 04:03:59 +0800

    complete Item Factory

--- src/main/java/lt/vu/mif/jate/tasks/task02/store/ItemFactory.java
+++ src/main/java/lt/vu/mif/jate/tasks/task02/store/ItemFactory.java
@@ -6,8 +6,10 @@
 package lt.vu.mif.jate.tasks.task02.store;
 
 import lt.vu.mif.jate.tasks.task02.store.model.Book;
+import lt.vu.mif.jate.tasks.task02.store.model.Item;
 import lt.vu.mif.jate.tasks.task02.store.model.Movie;
 import lt.vu.mif.jate.tasks.task02.store.model.WrongItemFormatException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Arrays;
@@ -26,13 +28,15 @@ public class ItemFactory {
         }
         return new Book.Builder(
                 bookJsonObject.getBigInteger("isbn"),
-                bookJsonObject.getString("title"),
-                bookJsonObject.getString("description"))
-                .categories(bookJsonObject.getJSONArray("categories"))
-                .rating(bookJsonObject.getDouble("averageRating"), bookJsonObject.getInt("ratingsCount"))
-                .authors(bookJsonObject.getJSONArray("authors"))
-                .pages(bookJsonObject.getInt("pageCount"))
-                .publisher(bookJsonObject.getString("publisher"))
+                bookJsonObject.has("title") ? bookJsonObject.getString("title") : null,
+                bookJsonObject.has("description") ? bookJsonObject.getString("description") : null)
+                .categories(bookJsonObject.has("categories") ? bookJsonObject.getJSONArray("categories") : new JSONArray("[\"Book\"]"))
+                .rating(bookJsonObject.has("averageRating") ? bookJsonObject.getDouble("averageRating") : 0,
+                        bookJsonObject.has("ratingsCount") ? bookJsonObject.getInt("ratingsCount") : 0)
+                .subtitle(bookJsonObject.has("subtitle") ? bookJsonObject.getString("subtitle") : null)
+                .publisher(bookJsonObject.has("publisher") ? bookJsonObject.getString("publisher") : null)
+                .pages(bookJsonObject.has("pageCount") ? bookJsonObject.getInt("pageCount") : 0)
+                .authors(bookJsonObject.has("authors") ? bookJsonObject.getJSONArray("authors") : new JSONArray())
                 .build();
     }
 
@@ -40,11 +44,17 @@ public class ItemFactory {
         if (!movieJsonObject.has("id")) {
             throw new WrongItemFormatException();
         }
-        return new Movie.Builder(movieJsonObject.getBigInteger("id"),
-                movieJsonObject.getString("name"),
-                movieJsonObject.getString("longDescription"))
-                .categories(movieJsonObject.getString("categoryPath"))
-                .rating(movieJsonObject.getDouble("customerRating"), movieJsonObject.getInt("numReviews"))
+        return new Movie.Builder(
+                movieJsonObject.getBigInteger("id"),
+                movieJsonObject.has("name") ? movieJsonObject.getString("name") : null,
+                movieJsonObject.has("longDescription") ? movieJsonObject.getString("longDescription") : null)
+                .categories(movieJsonObject.has("categoryPath") ? movieJsonObject.getString("categoryPath").replace("Movies/", "") : "/")
+                .rating(movieJsonObject.has("customerRating") ? movieJsonObject.getDouble("customerRating") : 0,
+                        movieJsonObject.has("numReviews") ? movieJsonObject.getInt("numReviews") : 0)
                 .build();
+    }
+
+    public static Item createItem(JSONObject jsonObject) {
+        return jsonObject.has("isbn") ? createBook(jsonObject) : createMovie(jsonObject);
     }
 }
