afb1de58ebd18c41be03a52ef23ad4abd6c3915e
Artem Popov <aptem336@gmail.com>
2020-05-15 01:58:15 +0800

    complete Negation

--- src/main/java/lt/vu/mif/jate/tasks/task02/search/operation/Negation.java
+++ src/main/java/lt/vu/mif/jate/tasks/task02/search/operation/Negation.java
@@ -1,10 +1,16 @@
 package lt.vu.mif.jate.tasks.task02.search.operation;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class Negation implements Operation {
 
     private final Operation operation;
-	
+
+    @Override
+    public boolean apply(Object o) {
+        return !operation.apply(o);
+    }
 }
