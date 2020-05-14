cccbacc002d0f38a534751c53f4778a2a4b40849
Artem Popov <aptem336@gmail.com>
2020-05-15 04:49:34 +0800

    complete Field, SearchManager

--- src/main/java/lt/vu/mif/jate/tasks/task02/search/SearchManager.java
+++ src/main/java/lt/vu/mif/jate/tasks/task02/search/SearchManager.java
@@ -1,39 +1,53 @@
 package lt.vu.mif.jate.tasks.task02.search;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.SortedSet;
 import lt.vu.mif.jate.tasks.task02.search.operation.Operation;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Search Manager to be extended by domain class.
- * @author valdo
+ *
  * @param <T> collection element type.
+ * @author valdo
  */
 public abstract class SearchManager<T extends Comparable<T>> {
-    
     /**
      * Get collection of elements to search in.
+     *
      * @return collection.
      */
     protected abstract Collection<T> getSearchItems();
-    
+
     /**
      * Search for items in collection.
+     *
      * @param operation operation for the search.
      * @return found elements.
      */
     public final SortedSet<T> findItems(final Operation operation) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
-    /**
-     * Return the list of unique values indicated by the keywords.
-     * @param field field to search.
-     * @return set of values.
-     */
-    public final Set<String> getValueList(final String field) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return getSearchItems().stream().filter(operation::apply).collect(Collectors.toCollection(TreeSet::new));
     }
 
+    /**
+     * Return the list of unique values indicated by the keywords.
+     *
+     * @param name fieldNameAnnotation to search.
+     * @return set of values.
+     */
+    public final Set<String> getValueList(final String name) {
+        Set<String> result = new HashSet<>();
+        for (T item : getSearchItems()) {
+            Object fieldValue = new lt.vu.mif.jate.tasks.task02.search.operation.operand.Field(name).getValue(item);
+            if (fieldValue instanceof Set) {
+                for (String setValue : (Set<String>) fieldValue) {
+                    result.add(setValue.matches("[A-Z]*") ? setValue.substring(0, 1).toUpperCase(Locale.ROOT)
+                            + setValue.toLowerCase(Locale.ROOT).substring(1) : setValue);
+                }
+            } else if (fieldValue != null) {
+                result.add(fieldValue.toString());
+            }
+        }
+        return result;
+    }
 }
--- src/main/java/lt/vu/mif/jate/tasks/task02/search/operation/operand/Field.java
+++ src/main/java/lt/vu/mif/jate/tasks/task02/search/operation/operand/Field.java
@@ -2,6 +2,7 @@ package lt.vu.mif.jate.tasks.task02.search.operation.operand;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lt.vu.mif.jate.tasks.task02.search.SearchManager;
 import lt.vu.mif.jate.tasks.task02.search.Searchable;
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -20,20 +21,10 @@ public class Field implements Operand {
     private final String name;
 
     @Override
-    public Object getValue(Object object) {
-        Class<?> itemClass = object.getClass();
-        java.lang.reflect.Field[] fields = new java.lang.reflect.Field[0];
-        do {
-            fields = ArrayUtils.addAll(fields, itemClass.getDeclaredFields());
-            itemClass = itemClass.getSuperclass();
-        } while (itemClass.getSuperclass() != null);
-        for (java.lang.reflect.Field field : fields) {
+    public Object getValue(Object item) {
+        for (java.lang.reflect.Field field : getFields(item.getClass())) {
             try {
-                AccessController.doPrivileged((PrivilegedAction<java.lang.reflect.Field>) () -> {
-                    field.setAccessible(true);
-                    return null;
-                });
-                Object fieldValue = field.get(object);
+                Object fieldValue = field.get(item);
                 if (fieldValue != null) {
                     Searchable searchable = field.getAnnotation(Searchable.class);
                     if (searchable != null) {
@@ -47,5 +38,20 @@ public class Field implements Operand {
             }
         }
         return null;
+    }
+
+    private static java.lang.reflect.Field[] getFields(Class<?> itemClass) {
+        java.lang.reflect.Field[] fields = new java.lang.reflect.Field[0];
+        do {
+            fields = ArrayUtils.addAll(fields, itemClass.getDeclaredFields());
+            itemClass = itemClass.getSuperclass();
+        } while (itemClass.getSuperclass() != null);
+        for (java.lang.reflect.Field field : fields) {
+            AccessController.doPrivileged((PrivilegedAction<java.lang.reflect.Field>) () -> {
+                field.setAccessible(true);
+                return null;
+            });
+        }
+        return fields;
     }
 }
